26f4fd5228750f50c01592b7175c935227802047
Saurabh Tripathy <lordsaurabh.tripathy>
2021-05-04 08:16:27 +0530

    test

--- my.js
+++ my.js
@@ -1,6 +1,12 @@
 function test(){
     const url = "https://script.google.com/macros/s/AKfycbx1bdTnUL1BBh3hMVzl2yFu2dVTz92OwBfB_goAbk222ziay_5UFt2dKhlIpladAy54HA/exec";
-    fetch(url)
+    fetch(url,{
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-Type': 'text/plain;charset=utf-8',
+        }
+    })
         .then(d=> d.json())
         .then(d =>{
             document.getElementById("app").textContent = d[0].status;    
