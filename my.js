cd0b2126f70c4b8318e56de0da14acfa97cc8d11
Saurabh Tripathy <lordsaurabh.tripathy>
2021-05-04 08:19:40 +0530

    test

--- my.js
+++ my.js
@@ -2,9 +2,12 @@ function test(){
     const url = "https://script.google.com/macros/s/AKfycbx1bdTnUL1BBh3hMVzl2yFu2dVTz92OwBfB_goAbk222ziay_5UFt2dKhlIpladAy54HA/exec";
     fetch(url,{
         method: 'POST',
-        body: data,
-        headers: {
-            'Content-Type': 'text/plain;charset=utf-8',
+    data: data,
+    mode: 'cors',
+    credentials: 'include', // include, *same-origin, omit
+    redirect: 'follow',
+    headers: {
+        'Content-Type': 'text/plain;charset=utf-8',
         }
     })
         .then(d=> d.json())
